937226319e2fa174f9049e9a6ec69f5ded26b236
ariaenggar <aria.enggar@gmail.com>
2021-12-12 22:46:35 +0700

    finish

---
+++ Assets/Scripts/CommandPattern/Command.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command
+{
+    public abstract void Execute();
+    public abstract void UnExecute();
+}
+
---
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+    [SerializeField]
+    MonoBehaviour factory;
+    IFactory Factory { get { return factory as IFactory; } }
+    
+    void Start ()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn ()
+    {
+        if (playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+        // Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        
+        int spawnEnemy = Random.Range(0, 3);
+
+        //Memduplikasi enemy
+        Factory.FactoryMethod(spawnEnemy);
+
+    }
+}
